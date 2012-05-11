7b30e07e68d6174c8924f51d51eccae07d20a6f7
Lupo Montero <lupo@e-noise.com>
2012-05-10 20:03:52 -0500

    Initial commit

---
+++ test/test-backbone-validator.js
@@ -0,0 +1,51 @@
+var
+  Backbone = require('backbone'),
+  assert = require('assert'),
+  vows = require('vows'),
+  suite = vows.describe('backbone-validator'),
+  validator = require(__dirname + '/../lib/backbone-validator'),
+
+  MyModel = Backbone.Model.extend({
+    validate: validator.create({
+      type: { equal: 'user' },
+      firstname: { type: 'string' },
+      lastname: { type: 'string' },
+      email: { type: 'email' }
+    })
+  });
+
+suite.addBatch({
+  'validate all good': {
+    topic: function () {
+      var foo = new MyModel({
+        type: 'user',
+        firstname: 'Lupo',
+        lastname: 'Montero',
+        email: 'lupo@e-noise.com'
+      });
+
+      this.callback(null, foo.validate(foo.toJSON()));
+    },
+    'validation returns undefined': function (er, ret) {
+      assert.ok(ret === undefined);
+    }
+  },
+
+  'validate bad email': {
+    topic: function () {
+      var foo = new MyModel({
+        type: 'user',
+        firstname: 'Lupo',
+        lastname: 'Montero',
+        email: 'not an email'
+      });
+
+      this.callback(null, foo.validate(foo.toJSON()));
+    },
+    'validation returns undefined': function (er, ret) {
+      assert.ok(ret.message === 'Attribute "email" must be a of type email');
+    }
+  }
+});
+
+suite.export(module);
